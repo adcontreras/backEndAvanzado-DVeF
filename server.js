8d17cb5ff9cdf93ee56f38f3ce5dccd87714dec8
adcontreras <adcontrerasmm@gmail.com>
2024-02-29 19:44:46 -0600

    creacion de npm y archivo server

---
+++ server.js
@@ -0,0 +1,17 @@
+// 1. importar Expressi, 
+//2.crear app con xpress, 
+//3.usar app.listen para abrir puertos
+
+// 1. importar Express
+import express from 'express';
+
+//2.crear app con xpress, 
+const api = express();
+
+//3.usar app.listen para abrir puertos
+api.listen(8000, ()=>{
+    console.log('API corriendo en el puerto 8000');
+});
+
+
+
